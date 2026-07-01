98856d4a7ef7c0f12ee2dd9d47cb7664809e51ec
ctkrug <ctkrug4501@gmail.com>
2026-07-01 16:29:26 -0600

    chore(lint): allow the performance global in app code
    main.js now timestamps animations with performance.now(); it was
    previously only allowed in tests.

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,7 @@ export default [
         clearInterval: 'readonly',
         URLSearchParams: 'readonly',
         navigator: 'readonly',
+        performance: 'readonly',
       },
     },
     rules: {
